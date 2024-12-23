f80667d6333470bb2317e932501ff34940458a9f
KingJoshua <kingjayeriughe@gmail.com>
2024-12-23 16:12:50 +0100

    UPDATE: update database configuration

--- App/Controllers/configs/db.php
+++ App/Controllers/configs/db.php
@@ -1,21 +1,11 @@
 <?php 
 // Check if it running on a local server
-if ($_SERVER['SERVER_NAME'] === 'localhost') {
-    return [
-        'host' => 'localhost',
-        'port' => 3306,
-        'dbname' => 'grab_back_db',
-        'username' => 'root',
-        'password' => '',
-    ];
-} else {
-    // Check if it is running on the production server
-    return [
-        'host' => 'localhost',
-        'port' => 3306,
-        'dbname' => 'grab_back_db',
-        'username' => 'grabback',
-        'password' => '3f#2!U7G1HwzYe',
-    ];
-}
+return [
+    'host' => 'localhost',
+    'port' => 3307,
+    'dbname' => 'ib_fyp_db',
+    'username' => 'root',
+    'password' => '',
+];
+
 
--- configs/db.php
+++ configs/db.php
@@ -1,21 +1,11 @@
 <?php 
 // Check if it running on a local server
-if ($_SERVER['REQUEST_URI'] === "/ib-fyp/") {
-    return [
-        'host' => 'localhost',
-        'port' => 3306,
-        'dbname' => 'ib_fyp_db',
-        'username' => 'root',
-        'password' => '',
-    ];
-} else {
-    // Check if it is running on the production server
-    return [
-        'host' => 'localhost',
-        'port' => 3307,
-        'dbname' => 'ib_fyp_db',
-        'username' => 'root',
-        'password' => '',
-    ];
-}
+return [
+    'host' => 'localhost',
+    'port' => 3307,
+    'dbname' => 'ib_fyp_db',
+    'username' => 'root',
+    'password' => '',
+];
+
 
