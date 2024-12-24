060edbb91e96b284f7c59eb5b4b3f7ffe03cb725
KingJoshua <kingjayeriughe@gmail.com>
2024-12-24 13:38:49 +0100

    FEAT: add instance for invalid credentials

--- App/views/partials/message.php
+++ App/views/partials/message.php
@@ -8,3 +8,14 @@
         </div>
     <?php endforeach; ?>
 <?php endif; ?>
+
+<?php if ($errorMessages): ?>
+    <?php foreach ($errorMessages as $errorMessage): ?>
+        <div class="alert alert-danger alert-dismissible" role="alert">
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            <div class="alert-message">
+                <strong><?=$errorMessage ?></strong>
+            </div>
+        </div>
+    <?php endforeach; ?>
+<?php endif; ?>
