2233ef16c16c81457112888b77cfba80cf9b17dc
KingJoshua <kingjayeriughe@gmail.com>
2024-12-27 16:28:57 +0100

    UPDATE: change port

--- configs/db.php
+++ configs/db.php
@@ -2,7 +2,7 @@
 // Check if it running on a local server
 return [
     'host' => 'localhost',
-    'port' => 3307,
+    'port' => 3306,
     'dbname' => 'ib_fyp_db',
     'username' => 'root',
     'password' => '',
