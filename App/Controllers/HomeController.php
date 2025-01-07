a9091febeeeb9fa89d6200ad46bd40acccb6afb3
KingJoshua <kingjayeriughe@gmail.com>
2025-01-07 13:42:11 +0100

    FEAT: insert investigator name in database

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -73,7 +73,8 @@ class HomeController
     }
     public function dashboard()
     {
-        loadView('home');
+        $originalCaseFiles = $this->db->query("SELECT * FROM analysis_results")->fetchAll();
+        loadView('home', ["originalCaseFiles" => $originalCaseFiles]);
         Session::clearAll();
     }
     public function uploadOriginal()
@@ -128,7 +129,6 @@ class HomeController
     }
     public function analyse_stage_one()
     {
-        var_dump($_POST);
         // DEFINING THE VALID FILE TYPES
         // 0: .txt file
         // 1: .pdf file
@@ -139,12 +139,14 @@ class HomeController
         $stage_one_file = $_FILES['stage_one_file']['name'];
         $stage_one_file_type = $_FILES['stage_one_file']['type'];
         $original_case_file_ID = $_POST['select_original_case_file'];
+        $investigator_name = $_POST['investigator_name'];
 
         if (in_array($stage_one_file_type, $allowed_types)) {
             // IF THE UPLOADED FILE IS VALID
             $stage_one_file_hash = hash('sha512', $stage_one_file);
             Session::set('stage_one_file_hash', $stage_one_file_hash);
             Session::set('original_case_file_ID', $original_case_file_ID);
+            Session::set('investigator_name', $investigator_name);
         } else {
             // REDIRECTING THE USER TO THE DASHBOARD
             redirect('http://localhost/ib-fyp/dashboard/analysis/stage-one');
@@ -200,6 +202,7 @@ class HomeController
         $stageTwoScore = 0;
         $stageThreeScore = 0;
         $originalCaseFileID = Session::get('original_case_file_ID');
+        $investigator_name = Session::get('investigator_name');
 
         $originalFileHash = $this->db->query("SELECT * FROM original_case_files WHERE id = $originalCaseFileID")->fetch()->file_hash;
 
@@ -225,9 +228,10 @@ class HomeController
             "stage_one_results" => $stageOneScore,
             "stage_two_results" => $stageTwoScore,
             "stage_three_results" => $stageThreeScore,
-            "analysis_result" => $analysisResult
+            "analysis_result" => $analysisResult,
+            "investigator_name" => $investigator_name
         ];
-        $this->db->query("INSERT INTO `analysis_results` (`stage_one_results`, `stage_two_results`, `stage_three_results`, `analysis_result`) VALUES (:stage_one_results, :stage_two_results, :stage_three_results, :analysis_result)", $params);
+        $this->db->query("INSERT INTO `analysis_results` (`stage_one_results`, `stage_two_results`, `stage_three_results`, `analysis_result`, `investigator_name`) VALUES (:stage_one_results, :stage_two_results, :stage_three_results, :analysis_result, :investigator_name)", $params);
         Session::set('stage_one_score', $stageOneScore);
         Session::set('stage_two_score', $stageTwoScore);
         Session::set('stage_three_score', $stageThreeScore);
