61578f08a3796d2878b0c1b527400a4eda141fd1
KingJoshua <kingjayeriughe@gmail.com>
2025-01-24 11:23:49 +0100

    UPDATE: database port

--- configs/db.php
+++ configs/db.php
@@ -2,7 +2,7 @@
 // Check if it running on a local server
 return [
     'host' => 'localhost',
-    'port' => 3306,
+    'port' => 3307,
     'dbname' => 'ib_fyp_db',
     'username' => 'root',
     'password' => '',
